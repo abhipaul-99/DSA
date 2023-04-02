592d646eeb4ca526b7246b83be6588aead78c82c
abhipaul-99 <reachtoabhishekpaul@gmail.com>
2023-04-02 14:07:23 +0530

    Time: 270 ms (33.99%), Space: 99.9 MB (31.79%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -4,7 +4,11 @@ public:
         sort(potions.begin(), potions.end());
         vector<int> res;
         for (int a: spells) {
-            long need = (success + a - 1) / a;
+            long need;
+            if (success % a!= 0)
+                need = 1 + success/a;
+            else
+                need = success/a;
             auto it = lower_bound(potions.begin(), potions.end(), need);
             res.push_back(potions.end() - it);
         }
